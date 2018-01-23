e8609ea82115668af8a2965bf3b6255885987e46
Dan Sheikh <dan.sheikh@bcgdv.com>
2018-01-22 18:00:37 -0800

    Setup test profile.

---
+++ src/main/java/com/bcg/dv/configurations/DatabaseConfiguration.java
@@ -0,0 +1,39 @@
+package com.bcg.dv.configurations;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DatabaseConfiguration {
+	
+	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
+	
+	@Bean
+	@Profile("test")
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		final LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactory.setDataSource(dataSource());
+		entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		return entityManagerFactory;
+	}
+	
+	@Bean
+	@Profile("test")
+	public JpaTransactionManager transactionManager(final EntityManagerFactory entityManagerFactory) {
+		final JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory);
+		return transactionManager;		
+	}
+}
