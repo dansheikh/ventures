3393688b5eb06afd0bddb937eac8974e78fb5c3f
Danish Sheikh <danish.sheikh@capgroup.com>
2018-01-23 17:34:24 -0800

    Modified schema setup. Added in-memory database details.

--- src/main/java/com/bcg/dv/configurations/DatabaseConfiguration.java
+++ src/main/java/com/bcg/dv/configurations/DatabaseConfiguration.java
@@ -1,13 +1,15 @@
 package com.bcg.dv.configurations;
 
+import java.util.Properties;
+
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -20,18 +22,25 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class DatabaseConfiguration {
 	
+	@Value("${hibernate.dialect}")
+	private String hibernateDialect;
+	
+	@Value("${hibernate.hbm2ddl.auto}")
+	private String hibernateHbm2Ddl;
+	
+	@Value("${hibernate.show_sql}")
+	private String hibernateShowSql;
+	
 	@Bean
 	@Profile("test")
 	public DataSource dataSource() {
-		EmbeddedDatabase db = new EmbeddedDatabaseBuilder()
+		return new EmbeddedDatabaseBuilder()
 				.setName("esi")
 				.setType(EmbeddedDatabaseType.H2)
 				.setScriptEncoding("UTF-8")
 				.ignoreFailedDrops(true)
-				.addScripts("schema.sql", "data.sql")
+				.addDefaultScripts()
 				.build();
-
-		return db;
 
 		// return DataSourceBuilder.create().build();
 	}
@@ -39,11 +48,22 @@ public class DatabaseConfiguration {
 	@Bean
 	@Profile("test")
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		final HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
 		final LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		final Properties jpaVendorProperties = new Properties();
+		
+		jpaVendorProperties.setProperty("hibernate.hbm2ddl.auto", hibernateHbm2Ddl);
+		jpaVendorProperties.setProperty("hibernate.dialect", hibernateDialect);
+		jpaVendorProperties.setProperty("hibernate.show_sql", hibernateShowSql);
+		
+		jpaVendorAdapter.setGenerateDdl(false);
+
 		factory.setDataSource(dataSource());
-		factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		factory.setJpaVendorAdapter(jpaVendorAdapter);
+		factory.setJpaProperties(jpaVendorProperties);
 		factory.setPackagesToScan("com.bcg.dv.entities");
 		factory.afterPropertiesSet();
+		
 		return factory;
 	}
 	
