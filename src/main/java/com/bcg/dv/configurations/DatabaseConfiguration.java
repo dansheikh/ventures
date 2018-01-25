ee4cbe36389656892d2adc7dc2d2fa04a00dbf61
Danish Sheikh <danish.sheikh@capgroup.com>
2018-01-24 16:41:38 -0800

    Applied formatting.

--- src/main/java/com/bcg/dv/configurations/DatabaseConfiguration.java
+++ src/main/java/com/bcg/dv/configurations/DatabaseConfiguration.java
@@ -1,10 +1,8 @@
 package com.bcg.dv.configurations;
 
 import java.util.Properties;
-
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,55 +19,51 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = {"com.bcg.dv.repositories"})
 @EnableTransactionManagement
 public class DatabaseConfiguration {
-	
-	@Value("${hibernate.dialect}")
-	private String hibernateDialect;
-	
-	@Value("${hibernate.hbm2ddl.auto}")
-	private String hibernateHbm2Ddl;
-	
-	@Value("${hibernate.show_sql}")
-	private String hibernateShowSql;
-	
-	@Bean(destroyMethod = "shutdown")
-	@Profile("test")
-	public DataSource dataSource() {
-		return new EmbeddedDatabaseBuilder()
-				.setName("esi")
-				.setType(EmbeddedDatabaseType.H2)
-				.setScriptEncoding("UTF-8")
-				.ignoreFailedDrops(true)
-				.addDefaultScripts()
-				.build();
-	}
-	
-	@Bean
-	@Profile("test")
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-		final HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
-		final LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-		final Properties jpaVendorProperties = new Properties();
-		
-		jpaVendorProperties.setProperty("hibernate.hbm2ddl.auto", hibernateHbm2Ddl);
-		jpaVendorProperties.setProperty("hibernate.dialect", hibernateDialect);
-		jpaVendorProperties.setProperty("hibernate.show_sql", hibernateShowSql);
-		
-		jpaVendorAdapter.setGenerateDdl(false);
 
-		factory.setDataSource(dataSource());
-		factory.setJpaVendorAdapter(jpaVendorAdapter);
-		factory.setJpaProperties(jpaVendorProperties);
-		factory.setPackagesToScan("com.bcg.dv.entities");
-		factory.afterPropertiesSet();
-		
-		return factory;
-	}
-	
-	@Bean
-	@Profile("test")
-	public JpaTransactionManager transactionManager(final EntityManagerFactory entityManagerFactory) {
-		final JpaTransactionManager transactionManager = new JpaTransactionManager();
-		transactionManager.setEntityManagerFactory(entityManagerFactory);
-		return transactionManager;		
-	}
+  @Value("${hibernate.dialect}")
+  private String hibernateDialect;
+
+  @Value("${hibernate.hbm2ddl.auto}")
+  private String hibernateHbm2Ddl;
+
+  @Value("${hibernate.show_sql}")
+  private String hibernateShowSql;
+
+  @Bean(destroyMethod = "shutdown")
+  @Profile("test")
+  public DataSource dataSource() {
+    return new EmbeddedDatabaseBuilder().setName("esi").setType(EmbeddedDatabaseType.H2)
+        .setScriptEncoding("UTF-8").ignoreFailedDrops(true).addDefaultScripts().build();
+  }
+
+  @Bean
+  @Profile("test")
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    final HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+    final LocalContainerEntityManagerFactoryBean factory =
+        new LocalContainerEntityManagerFactoryBean();
+    final Properties jpaVendorProperties = new Properties();
+
+    jpaVendorProperties.setProperty("hibernate.hbm2ddl.auto", hibernateHbm2Ddl);
+    jpaVendorProperties.setProperty("hibernate.dialect", hibernateDialect);
+    jpaVendorProperties.setProperty("hibernate.show_sql", hibernateShowSql);
+
+    jpaVendorAdapter.setGenerateDdl(false);
+
+    factory.setDataSource(dataSource());
+    factory.setJpaVendorAdapter(jpaVendorAdapter);
+    factory.setJpaProperties(jpaVendorProperties);
+    factory.setPackagesToScan("com.bcg.dv.entities");
+    factory.afterPropertiesSet();
+
+    return factory;
+  }
+
+  @Bean
+  @Profile("test")
+  public JpaTransactionManager transactionManager(final EntityManagerFactory entityManagerFactory) {
+    final JpaTransactionManager transactionManager = new JpaTransactionManager();
+    transactionManager.setEntityManagerFactory(entityManagerFactory);
+    return transactionManager;
+  }
 }
